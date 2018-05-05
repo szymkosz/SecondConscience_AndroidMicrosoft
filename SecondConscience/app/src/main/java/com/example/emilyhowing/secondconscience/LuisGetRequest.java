9b71d84b483c6e53a68d628f65531b39f22cc4ac
szymkosz <szymkosz@icloud.com>
2018-05-05 06:59:24 -0700

    Removed some unused statements

--- SecondConscience/app/src/main/java/com/example/emilyhowing/secondconscience/LuisGetRequest.java
+++ SecondConscience/app/src/main/java/com/example/emilyhowing/secondconscience/LuisGetRequest.java
@@ -7,12 +7,9 @@ package com.example.emilyhowing.secondconscience;
 import android.text.TextUtils;
 
 import java.io.BufferedReader;
-import java.io.DataOutputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-
-import javax.net.ssl.HttpsURLConnection;
 
 public class LuisGetRequest {
 
